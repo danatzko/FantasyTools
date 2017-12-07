fb18ceb4f9e96dc7ad7afde1966dcd184fc3b7e3
David Danatzko <ddanatzko@yahoo.com>
2017-12-07 12:59:49 -0500

    Updated Data model to include 2 new tables for tracking situational individual and team performance

--- scraper/management/commands/extract_salaries.py
+++ scraper/management/commands/extract_salaries.py
@@ -1,10 +1,12 @@
 from django.core.management.base import BaseCommand, CommandError
 import csv
 import os
-from stats.models import GameStats
-
+from stats.models import GameStats,Player,Team
+'''
+    The purpose of this script is to collect the game-by-game point and salary totals for the 2017 season
+    Where the objects don't exist, they will be created.
+'''
 CSV_FILE = os.path.join(os.path.expanduser("~"), "Projects/dk_salaries_2017.csv")
-
 
 class Command(BaseCommand):
     
@@ -12,25 +14,45 @@ class Command(BaseCommand):
         with open(CSV_FILE, newline='') as csvfile:
             my_reader = csv.DictReader(csvfile)
             for row in my_reader:
-                salary = row['salary']
-                if salary == '':
-                    salary = float(0.0)
+                # Salary has an odd format.  Needs to be cast as a float
+                salary = float(0.0) if row['salary'] == '' else float(row['salary'])
+
+                # Create all the objects that don't exist
+
+                player,created3 = Player.objects.get_or_create(name=row['name'])
+                player.points = player.points + row['points']
+                player.games = players.games + 1
+                player.avg_points_game = player.points / player.games
+                # Player averages should really be run as a second job.  Jupyter Notebook for demo purposes?
+                # player.avg_salary won't be accurate until all of the game stats are complete
+
+
+                # row['team'] indicates which team scored, 
+                # row['h_a'] indicates venue of score and team's relationship to it
+                if row['h_a'] == 'h':
+                    home,created = Team.objects.get_or_create(name=row['team'])
+                    away,created2 = Team.objects.get_or_create(name=row['oppt'])
                 else:
-                    salary = float(salary)
+                    home,created = Team.objects.get_or_create(name=row['oppt'])
+                    away,created2 = Team.objects.get_or_create(name=row['team'])
 
                 stat = GameStats(
+                    Player_id=player.id,
+                    Home=home,
+                    Away=away,
+                    salary=salary,
+                    pos=row['pos'],
+                    points=row['points'],
                     week=row['week'],
                     year=row['year'],
-                    name=row['name'],
-                    pos=row['pos'],
-                    team=row['team'],
-                    oppt=row['oppt'],
-                    salary=salary,
-                    points=row['points'],
-                    h_a=row['h_a'],
                 )
+                # Attempt to store the data to the DB.  Otherwise, stop the program
+                # and print the offending row.
                 try:
                     stat.save()
+                    #self.stdout.write(player.name)
+                    #self.stdout.write(home.name)
+                    #self.stdout.write(away.name)
                 except ValueError:
                     print(row)
                     break
--- stats/models.py
+++ stats/models.py
@@ -3,13 +3,49 @@ from django.db import models
 # Create your models here.
 
 class GameStats(models.Model):
+    
+    Player = models.ForeignKey('Player',
+        on_delete=models.DO_NOTHING,
+        null=False,
+        default=None,
+    )
+
+    Home = models.ForeignKey('Team',
+        on_delete=models.DO_NOTHING,
+        null=False,
+        default=None,
+    )  
+    Away = models.ForeignKey('Team',
+        on_delete=models.DO_NOTHING,
+        null=False,
+        default=None,
+        related_name="OpposingTeam"
+    )
     week = models.IntegerField(default=0)
     year = models.IntegerField(default=0)
-    name = models.CharField(max_length=50)
     pos = models.CharField(max_length=5)
-    team = models.CharField(max_length=5)
-    oppt = models.CharField(max_length=5)
     salary = models.FloatField(default=0)
-    h_a = models.CharField(max_length=1)
     points = models.FloatField(default=0)
+
+class Player(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    games = models.IntegerField(default=0)
+    plays = models.IntegerField(default=0)
+    points = models.IntegerField(default=0)
+    avg_salary = models.IntegerField(default=0)
+    avg_points_game = models.IntegerField(default=0)
+    avg_points_away = models.IntegerField(default=0)
+    avg_points_home = models.IntegerField(default=0)
+    deviation = models.IntegerField(default=0)
+
+class Team(models.Model):
+    name = models.CharField(max_length=5, unique=True)
+    o_points = models.IntegerField(default=0)
+    d_points = models.IntegerField(default=0)
+    o_points_away = models.IntegerField(default=0)
+    o_points_home = models.IntegerField(default=0)
+    d_points_away = models.IntegerField(default=0)
+    d_points_home = models.IntegerField(default=0)
+    a_deviation = models.IntegerField(default=0)
+    h_deviation = models.IntegerField(default=0)
     
